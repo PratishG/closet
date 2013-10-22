7e37dbd62d4fc7ed91fce224f82b6ffb7da2402b
tofr <mrt475@gmail.com>
2013-10-22 14:29:50 -0400

    shell for skeleton files

---
+++ skelshell.java
@@ -0,0 +1,55 @@
+//skeleton file shell
+
+public class _ {
+
+    /*=====================================
+      _ _(_) -- _
+      precondition:  
+      postcondition: 
+      =====================================*/
+    public _ _(  ) {
+	// your implementation here
+    }
+
+
+    /*=====================================
+      int _(_) -- _
+      precondition:  
+      postcondition: 
+      =====================================*/
+    public int _(  ) {
+	// your implementation here
+	return 0; //placeholder to get past compiler
+    }
+
+
+    /*=====================================
+      boolean _(_) -- _
+      precondition:  
+      postcondition: 
+      =====================================*/
+    public boolean _(  ) {
+	// your implementation here
+	return false; //placeholder to get past compiler
+    }
+
+
+    /*=====================================
+      String _(_) -- _
+      precondition:  
+      postcondition: 
+      =====================================*/
+    public String _(  ) {
+	// your implementation here
+	return ""; //placeholder to get past compiler
+    }
+
+
+    //main method for testing
+    public static void main( String[] args ) {
+
+	System.out.println();
+	System.out.println( "Testing ..." );
+    }
+
+} //end class
