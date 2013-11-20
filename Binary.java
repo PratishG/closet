ad5f040ae478c61ec425d1480804686b4bb712a7
mistertopher <mykolyk@stuycs.org>
2013-11-20 15:34:02 -0500

    update Binary skel

--- Binary.java
+++ Binary.java
@@ -1,15 +1,14 @@
 //skeleton file for class Binary
-// eventually should adhere to specifications of Comparable interface
 
-public class Binary {
+public class Binary implements Comparable {
 
     private int _decNum;
     private String _binNum;
 
     /*=====================================
       default constructor
-      precondition:  
-      postcondition: 
+      pre:  n/a
+      post: initializes _decNum to 0, _binNum to "0"
       =====================================*/
     public Binary() { 
 	// *** YOUR IMPLEMENTATION HERE ***
@@ -18,8 +17,8 @@ public class Binary {
 
     /*=====================================
       overloaded constructor
-      precondition:  n >= 0
-      postcondition: 
+      pre:  n >= 0
+      post: sets _decNum to n, _binNum to equiv string of bits
       =====================================*/
     public Binary( int n ) {
 	// *** YOUR IMPLEMENTATION HERE ***
@@ -28,8 +27,8 @@ public class Binary {
 
     /*=====================================
       String toString() -- returns String representation of this Object
-      precondition:  
-      postcondition: 
+      pre:  n/a
+      post: returns String of 1's and 0's representing value of this Object
       =====================================*/
     public String toString() { 
 	// *** YOUR IMPLEMENTATION HERE ***
@@ -39,9 +38,13 @@ public class Binary {
 
     /*=====================================
       String decToBin(int) -- converts base-10 input to binary
-      precondition:  n >= 0
-      postcondition: return String of bits
-      eg  decToBin(14) -> "1110"
+      pre:  n >= 0
+      post: returns String of bits
+      eg  decToBin(0) -> "0"
+          decToBin(1) -> "1"
+	  decToBin(2) -> "10"
+	  decToBin(3) -> "11"
+	  decToBin(14) -> "1110"
       =====================================*/
     public static String decToBin( int n ) {
 	// *** YOUR IMPLEMENTATION HERE ***
@@ -51,13 +54,41 @@ public class Binary {
 
     /*=====================================
       String decToBinR(int) -- converts base-10 input to binary, recursively
-      precondition:  n >= 0
-      postcondition: returns String of bits
-      eg  decToBin(14) -> "1110"
+      pre:  n >= 0
+      post: returns String of bits
+      eg  decToBinR(0) -> "0"
+          decToBinR(1) -> "1"
+	  decToBinR(2) -> "10"
+	  decToBinR(3) -> "11"
+	  decToBinR(14) -> "1110"
       =====================================*/
     public static String decToBinR( int n ) { 
 	// *** YOUR IMPLEMENTATION HERE ***
 	return ""; //placeholder to get past compiler
+    }
+
+
+    /*=============================================
+      boolean equals(Object) -- tells whether 2 Objs are equivalent
+      pre:  other is an instance of class Binary
+      post: Returns true if this and other are aliases (pointers to same 
+      Object), or if this and other represent equal binary values
+      =============================================*/
+    public boolean equals( Object other ) { 
+	// *** YOUR IMPLEMENTATION HERE ***
+	return false; //placeholder to get past compiler
+    }
+
+
+    /*=============================================
+      int compareTo(Object) -- tells which of two Binary objects is greater
+      pre:  other is instance of class Binary
+      post: Returns 0 if this Object is less than input Object,
+            negative integer if this < input, positive integer otherwise
+      =============================================*/
+    public int compareTo( Object other ) {
+	// *** YOUR IMPLEMENTATION HERE ***
+	return -1; //placeholder to get past compiler
     }
 
 
