02734135f86a4a6be336c69e4d0f550716213de8
tofr <mykolyk@stuycs.org>
2014-03-10 13:05:25 -0400

    clean up

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,29 +1,20 @@
 /*==================================================
   class GuessNumber -- fun fun fun!
 
-  eg, sample interaction with end user:
-  Guess a # fr 1-100: 50
+  eg, sample interaction with user:
+  java GuessNumber
+  Guess a number fr 1-100: 50
   Too high
-  Guess a # fr 1-49: 25
+  Guess a number fr 1-49: 25
   Too low
-  Guess a # fr 26-49: 38
+  Guess a number fr 26-49: 38
   Correct! It took 3 guesses
   ==================================================*/
-
-/*==================================================
-  the Breakdown:
-  Blah blah blah, yakkity smakkity, and a nice tall glass of OJ...
-  ==================================================*/
-
-
-import cs1.Keyboard; /* must have cs1 dir in same dir as this file 
-			for this to work */
 
 public class GuessNumber {
 
     //instance vars
     private int _lo, _hi, _guessCtr, _target;
-
 
 
     /*==================================================
@@ -33,29 +24,23 @@ public class GuessNumber {
             _guessCtr is 1, _target is random int on range [_lo,_hi]
       ==================================================*/
     public GuessNumber( int a, int b ) {
-	//*** YOUR IMPLEMENTATION HERE ***
     }
 
 
     /*==================================================
       void playRec() -- Prompts a user to guess until guess is correct.
                         Uses recursion.
-      pre:  
-      post: 
       ==================================================*/
     public void playRec() {
-	//*** YOUR IMPLEMENTATION HERE ***
     }
 
 
     /*==================================================
       void playIter() -- Prompts a user to guess until guess is correct.
                          Uses iteration.
-      pre:  
-      post: 
       ==================================================*/
     public void playIter() {
-	//*** YOUR IMPLEMENTATION HERE ***
+
     }
 
 
@@ -77,10 +62,4 @@ public class GuessNumber {
 	g.play();
     }
 
-
-    /*==================================================
-      _ _(_) -- 
-      pre:  
-      post: 
-      ==================================================*/
-}//end class
+}//end class GuessNumber
