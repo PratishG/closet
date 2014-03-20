26629976b48cb193597dc2f1c78aa2c93fe8af27
tofr <mykolyk@stuycs.org>
2014-03-20 15:25:45 -0400

    add LList v1

---
+++ LList01/LLNode.java
@@ -0,0 +1,8 @@
+/*****************************************************
+ * class LLNode
+ * Implements a node, for use in lists and other container classes.
+ *****************************************************/
+
+public class LLNode {
+
+}
---
+++ LList01/LList.java
@@ -0,0 +1,15 @@
+/*****************************************************
+ * class LList
+ * Implements a linked list.
+ *****************************************************/
+
+public class LList implements List { //your List.java must be in same dir
+
+    public boolean add( String x ) { } 
+
+    public String get( int i ) { } 
+
+    public String set( int i, String x ) { } 
+
+    public int size() { } 
+}
