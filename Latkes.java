045861b0be40728d6e8277312fdb1bd0e524e718
tofr <mykolyk@stuycs.org>
2014-04-03 15:54:51 -0400

    add Latkes skeleton

---
+++ Latkes.java
@@ -0,0 +1,26 @@
+/*****************************************************
+ * class Latkes
+ * Implements a stack of Strings using an array as underlying container.
+ *****************************************************/
+
+public class Latkes {
+
+    private String [] _stack;
+    private int _stackSize;
+
+    //constructor
+    public Latkes( int size ) { }
+
+    //means of insertion
+    public void push( String s ) { }
+
+    //means of removal
+    public String pop( ) { }
+
+    //chk for emptiness
+    public boolean isEmpty() { }
+
+    //chk for fullness
+    public boolean isFull() { }
+
+}//end class Latkes
