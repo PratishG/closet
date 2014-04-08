1df7551abcfdf84d2cd6f0f0fd105cdcae2a3d12
tofr <mykolyk@stuycs.org>
2014-04-08 13:02:10 -0400

    update stacks v2

--- stacksonstacks/v2/Latkes.java
+++ stacksonstacks/v2/Latkes.java
@@ -1,9 +1,6 @@
 /*****************************************************
  * class Latkes
  * Implements a stack of Strings using an array as underlying container.
- * New in Version 2: 
- * a) Adherence to specification; now implements Stack interface.
- * b) Fun new methods allMatched and flip! 
  *****************************************************/
 
 public class Latkes {
@@ -11,59 +8,94 @@ public class Latkes {
     private String [] _stack;
     private int _stackSize;
 
+
     //constructor
     public Latkes( int size ) { 
-	/*** YOUR IMPLEMENTATION HERE ***/
+	_stack = new String[size];
+	_stackSize = 0;
     }
 
 
     //means of insertion
-    //a user of this class should be able to make unlimited pushes
     public void push( String s ) { 
-	/*** YOUR IMPLEMENTATION HERE ***/
+	//if necessary, create more storage space
+	if ( isFull() ) {
+	    String[] temp = new String[ _stack.length * 2 ];	
+	    for( int i = 0; i < _stack.length; i++ ) {
+		temp[i] = _stack[i];
+	    }
+	    _stack = temp; //old _stack will be devoured by JGC
+	}
+	//add new element at end of array
+	//Q: Why is this more efficient than inserting at front?
+	_stack[_stackSize] = s;
+	_stackSize++;
     }
-
 
     //means of removal
     public String pop( ) { 
-	/*** YOUR IMPLEMENTATION HERE ***/
+	String retStr = "";
+	if ( isEmpty() )  return null;
+	retStr = _stack[ _stackSize---1 ];
+	return retStr;
     }
-
 
     //chk for emptiness
     public boolean isEmpty() { 
-	/*** YOUR IMPLEMENTATION HERE ***/
+	return _stackSize == 0; 
     }
-
 
     //chk for fullness
-    public boolean isFull() { 
-	/*** YOUR IMPLEMENTATION HERE ***/
+    public boolean isFull() {
+	return _stackSize >= _stack.length; 
     }
 
 
-    //return top element without popping it
-    public String peek() {
-	/*** YOUR IMPLEMENTATION HERE ***/
+    //main method for testing
+    public static void main( String[] args ) {
+
+	Latkes tastyStack = new Latkes(10);
+
+	tastyStack.push("aoo");
+	tastyStack.push("boo");
+	tastyStack.push("coo");
+	tastyStack.push("doo");
+	tastyStack.push("eoo");
+	tastyStack.push("foo");
+	tastyStack.push("goo");
+	tastyStack.push("hoo");
+	tastyStack.push("ioo");
+	tastyStack.push("joo");
+	tastyStack.push("coocoo");
+	tastyStack.push("cachoo");
+
+	//cachoo
+	System.out.println( tastyStack.pop() );
+	//coocoo
+	System.out.println( tastyStack.pop() );
+	//joo
+	System.out.println( tastyStack.pop() );
+	//ioo
+	System.out.println( tastyStack.pop() );
+	//hoo
+	System.out.println( tastyStack.pop() );
+	//goo
+	System.out.println( tastyStack.pop() );
+	//foo
+	System.out.println( tastyStack.pop() );
+	//eoo
+	System.out.println( tastyStack.pop() );
+	//doo
+	System.out.println( tastyStack.pop() );
+	//coo
+	System.out.println( tastyStack.pop() );
+	//boo
+	System.out.println( tastyStack.pop() );
+	//aoo
+	System.out.println( tastyStack.pop() );
+
+	//stack empty by now; SOP(null)
+	System.out.println( tastyStack.pop() );
     }
-
-
-    /**********************************************************
-     * precondition:  input string has length > 0
-     * postcondition: returns reversed string s
-     *                flip("desserts") -> "stressed"
-     **********************************************************/
-    public static String flip( String s ) { }
-
-
-    /**********************************************************
-     * precondition:  s contains only the characters {,},(,),[,]
-     * postcondition: allMatched( "( {} [()] )" ) -> true
-     *                allMatched( "([)]" )        -> false
-     *                allMatched( "" )            -> true
-     **********************************************************/
-    public static boolean allMatched( String s ) { }
-
-
 
 }//end class Latkes
---
+++ stacksonstacks/v2/StackWork.java
@@ -0,0 +1,28 @@
+/*****************************************************
+ * class StackWork
+ * Driver class for Latkes.
+ * Uses a stack to reverse a text string, check for sets of matching parens.
+ *****************************************************/
+
+public class StackWork {
+
+    /**********************************************************
+     * precondition:  input string has length > 0
+     * postcondition: returns reversed string s
+     *                flip("desserts") -> "stressed"
+     **********************************************************/
+    public static String flip( String s ) { }
+
+
+    /**********************************************************
+     * precondition:  s contains only the characters {,},(,),[,]
+     * postcondition: allMatched( "( {} [()] )" ) -> true
+     *                allMatched( "([)]" )        -> false
+     *                allMatched( "" )            -> true
+     **********************************************************/
+    public static boolean allMatched( String s ) { }
+
+
+    public static void main( String[] args ) { }
+
+}//end class StackWork
